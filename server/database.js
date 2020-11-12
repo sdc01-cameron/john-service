3fc4fa2ca4af3c9cc951738ce67df2a82deafa31
Deb Johnson <friendswithdrj@gmail.com>
2020-11-12 15:41:56 -0600

    Add connection/schema/model

---
+++ server/database.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // Set schema
+  const productSchema = new mongoose.Schema({
+    name:  String,
+    price: Number,
+    prime: Boolean,
+    returnable: Boolean,
+    flavor: String,
+    ingredients: String,
+    brand: String,
+    sensitivity: String,
+    ingredient_information: String,
+    about: String,
+    ratings: [{user: String, rating: Number, rating_date: Date}]
+  })
+  // compile schema to a model
+  const Product = mongoose.model('Product', productSchema);
+});
