698cf9fee449cb24e7fd36b5cd6c516666acd408
Deb Johnson <friendswithdrj@gmail.com>
2020-11-12 15:51:27 -0600

    Edit database connection

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
