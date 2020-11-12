889a05e6af786f885847d96a677533bf1f1292b2
Deb Johnson <friendswithdrj@gmail.com>
2020-11-12 16:21:22 -0600

    Add console log confirming db connection

--- server/database.js
+++ server/database.js
@@ -4,6 +4,7 @@ mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
+  console.log('Connect to MongoDB');
   // Set schema
   const productSchema = new mongoose.Schema({
     name:  String,
