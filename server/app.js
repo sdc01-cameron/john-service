cfca1937cea05c3d6c2c2eed92ba6de80a237e14
Deb Johnson <friendswithdrj@gmail.com>
2020-11-13 00:04:57 -0600

    Server returning product object to Postman

--- server/app.js
+++ server/app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3005;
 
+const Product = require('../server/database').Product;
+
+app.use(express.json());
+app.use(express.urlencoded());
 app.use('/', express.static('public'));
 
 app.get('/api/products/:id/product', (req, res) => {
-  res.send('Product info here');
+  Product.find((err, products) => {
+    if (err) {
+      console.log('Error: ', err);
+      res.send(404);
+    } else {
+      res.send(products[req.params.id]);
+    }
+  })
 });
 
 app.listen(port, () => {
