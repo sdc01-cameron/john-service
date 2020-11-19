10e2789f1c636241a8fbfcc77e2fa2bfa652d9f1
Deb Johnson <friendswithdrj@gmail.com>
2020-11-19 15:41:46 -0600

    Add product test/configure for enzyme

---
+++ spec/product.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Product from '../client/src/Product';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Product Component', () => {
+  it('should render', () => {
+    const product = {
+      "name": "Kind Bars, Salted Caramel",
+      "price": 5,
+      "prime": true,
+      "returnable": false,
+      "flavor": 5,
+      "ingredients": "Peanuts, almonds, chicory root fiber, cashews, pecans",
+      "brand": "Kind",
+      "sensitivity": "Gluten Free, Nut",
+      "ingredient_info": "Protein 3 grams",
+      "about": `Contains 12 - 1.4oz Bars Made with simple, whole ingredients With 5g of sugar, it's a satisfying, nutty snack that only seems indulgent. Gluten Free, no genetically modified ingredients, 0g Trans fat, Kosher Low glycemic index, low sodium, good source of fiber`,
+      "Ratings_avg": 5
+     }
+
+    const wrapper = shallow(<Product product={product}/>);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+})
