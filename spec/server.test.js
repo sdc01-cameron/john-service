60565a15965de24d768df0c9000578065ba6a119
Deb Johnson <friendswithdrj@gmail.com>
2020-11-19 21:28:17 -0600

    Add supertest to app/tests for server

---
+++ spec/server.test.js
@@ -0,0 +1,17 @@
+const supertest = require('supertest');
+
+const request = supertest('http://localhost:3005');
+
+describe('GET route for product', () => {
+  it('should successfully access GET route', (done) => {
+    request.get('/api/products/49').expect(200).expect(res => {
+      expect(res.status).toEqual(200);
+    }).end(done);
+  });
+
+  it('should return a 404 if the product id is not in the request', (done) => {
+    request.get('/api/product').expect(404).expect(res => {
+      expect(res.status).toEqual(404);
+    }).end(done);
+  });
+});
