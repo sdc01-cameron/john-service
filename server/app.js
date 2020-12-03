2e998884c0263c7a44041af6f6c681220d0230ea
John Anderton <janderton24@gmail.com>
2020-12-02 16:39:50 -0800

    makes changes to add post, put, and delete routes. also adds a new writeFileStream.js file to write a stream

--- server/app.js
+++ server/app.js
@@ -11,14 +11,66 @@ app.use('/', express.static('public'));
 app.get('/api/products/:id', (req, res) => {
   Product.findById(req.params.id, (err, product) => {
     if (err) {
-      console.log('Error: ', err);
+      console.log('Error in GET (single document):', err);
       res.send(404);
     } else {
+      console.log('Succes in Get (single Document');
       res.send(product);
     }
-  })
+  });
 });
+
+app.get('/api/getall', (req, res) => {
+  Product.find({}, (err, product) => {
+    if (err) {
+      console.log('Error in GET (full collection): ', err);
+      res.send(404);
+    } else {
+      console.log('Succes in Get (full collection)');
+      res.send(product);
+    }
+  });
+});
+
+app.post('/api/products/new', (req, res) => {
+  console.log('req.body is: ', req.body);
+  var newProduct = new Product(req.body);
+  newProduct.save((err, insertion) => {
+    if (err) {
+      console.log('Error in Post occurred: ', err);
+      res.send('Error in Post occurred');
+      return;
+    }
+    console.log('Success in Post');
+    res.send(insertion);
+  });
+});
+
+app.delete('/api/products/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (err, deletedRecord) => {
+    if (err) {
+      console.log('Error in Delete occurred: ', err);
+      res.send('Error in Delete occurred');
+      return;
+    }
+    console.log('Success in Delete: ');
+    res.send(deletedRecord);
+  });
+});
+
+app.put('/api/products/:id', (req, res) => {
+  Product.updateOne({_id: req.params.id}, req.body, (err, updatedRecord) => {
+    if (err) {
+      console.log('Error in Put occurred: ', err);
+      res.send('Error in Put occurred');
+      return;
+    }
+    console.log('Success in Put');
+    res.send(updatedRecord);
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
-})
+});
