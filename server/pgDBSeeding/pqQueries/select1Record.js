aceb0fd8660686b94259eb396059667dbe8f69b5
John Anderton <janderton24@gmail.com>
2020-12-09 14:26:40 -0800

    updates the get api route to use the postgres database

--- server/pgDBSeeding/pqQueries/select1Record.js
+++ server/pgDBSeeding/pqQueries/select1Record.js
@@ -1,16 +1,32 @@
 const pool = require('../pgConnection.js').pool;
 
-const id = 10000001;
-let preDate = new Date();
+// const id = 10000001;
 
-pool.query(`SELECT * FROM products WHERE id = ${id}`, (error, result) => {
-  if (error) {
-    console.log(error);
-  } else {
-    let postDate = new Date();
-    console.log('Query Time: ', (postDate - preDate));
-    console.log(`Success In Querying ${result.rows.length} with ID ${id}`);
-    console.log(result.rows);
-  }
-  pool.end;
-});
+const selectOne = (req, res) => {
+  const id = req.params.id;
+  pool.query(`SELECT * FROM products WHERE id = ${id}`, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.send(404);
+    } else {
+      console.log(`Success In Querying ${result.rows.length} with ID ${id}`);
+      console.log(result.rows);
+      res.send(result.rows);
+    }
+    pool.end;
+  });
+};
+
+// pool.query(`SELECT * FROM products WHERE id = ${id}`, (error, result) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     let postDate = new Date();
+//     console.log('Query Time: ', (postDate - preDate));
+//     console.log(`Success In Querying ${result.rows.length} with ID ${id}`);
+//     console.log(result.rows);
+//   }
+//   pool.end;
+// });
+
+module.exports.selectOne = selectOne;
